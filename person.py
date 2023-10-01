8a2369ad672662715a28759e22c582238b788b48
imxasik <99292014+imxasik@users.noreply.github.com>
2023-10-01 20:44:41 +0600

    Update person.py

--- person.py
+++ person.py
@@ -1,18 +1,16 @@
+import random
 import csv
 
-# Your code to generate CSV data here
+# Generate random person data
+data = []
+for _ in range(3):
+    name = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(5))
+    age = random.randint(18, 65)
+    height = round(random.uniform(150, 200), 2)
+    data.append([name, age, height])
 
-data = [
-    ["Name", "Age"],
-    ["Alice", 25],
-    ["Bob", 30],
-    # Add more data here
-]
-
-csv_file = "data/output.csv"
-
-with open(csv_file, mode="w", newline="") as file:
-    writer = csv.writer(file)
+# Save data to a CSV file
+with open('person.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Name', 'Age', 'Height'])
     writer.writerows(data)
-
-print("CSV file generated.")
